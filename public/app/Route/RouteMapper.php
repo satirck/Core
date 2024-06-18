d69662298af83e3ea9c811557f3eba9d576f2164
Mikita Shydlouski <retdarkw@gmail.com>
2024-06-18 11:42:10 +0300

    remove some code lines for adding 404 view in future

--- public/app/Route/RouteMapper.php
+++ public/app/Route/RouteMapper.php
@@ -363,15 +363,7 @@ class RouteMapper
         $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $method = $_SERVER['REQUEST_METHOD'];
 
-        try {
-            $this->dispatch($url, $method);
-        } catch (StatusErrorException | InvalidRouteArgumentException $e) {
-            http_response_code($e->getCode());
-            echo sprintf('Error with code [%d]! %s<br>', $e->getCode(), $e->getMessage());
-        }
-        catch (ReflectionException $e) {
-            http_response_code($e->getCode());
-            echo sprintf('Error with controllers reflections in url! %s<br>', $url);
-        }
+        $this->dispatch($url, $method);
+
     }
 }
